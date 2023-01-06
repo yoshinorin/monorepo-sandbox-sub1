9953d71be1af5164e86cbbad06c911640091a290
yoshinorin <yoshinorin.net@outlook.com>
2023-01-06 20:14:07 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 function print1() {
-  console.log("this is a sub pkg1")
+  console.log("this is a sub pkg1 - 1")
+}
+
+function print2() {
+  console.log("this is a sub pkg1 - 2")
 }
 
 export {
-  print1
+  print1,
+  print2
 }
